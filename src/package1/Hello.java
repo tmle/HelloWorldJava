fcb85b4e17e5e8863da3c27d5b1e220b081ecfbc
Thinh Le <thinhle@ThinhsMPMid2010.socal.rr.com>
2018-10-22 15:31:09 -0700

    add Good morning Vietnam

--- src/package1/Hello.java
+++ src/package1/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		for (int i=1; i<=5; i++) {
-			System.out.println("Hello World " + i);
+			System.out.println("Hello World");
+			System.out.println("Good morning Vietnam");
 		}
 	}
 }
